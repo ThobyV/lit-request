0d0e369856cfd5846d0d76b782ea8f15630c7678
Thoby V <Vivee18@gmail.com>
2025-05-04 18:01:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ MIT License 2020
 */
 
 export default (function create(options) {
-  function isObject(val) {
-    return typeof val == 'object';
-  }
+  const payload = 'payload';
 
-  function deepMerge(base, config) {
-    const newObj = Object.assign({}, base, config);
+  const isObject = (val) => typeof val == 'object';
+
+  const isString = (str) => typeof str === 'string';
+
+  const deepMerge = (base, config) => {
+    const newObj = Object.assign({}, base, isString(config) ? { url: config } : config);
+    console.log({ newObj })
     if (isObject(base) && isObject(config)) {
       for (const i in config) {
         if (Array.isArray(config[i])) {
@@ -17,7 +20,7 @@ export default (function create(options) {
         if (
           isObject(config[i]) &&
           !Array.isArray(config[i]) &&
-          config[i].constructor === Object
+          config[i]?.constructor === Object
         ) {
           if (!newObj[i]) {
             newObj[i] = {};
@@ -34,22 +37,20 @@ export default (function create(options) {
 
   const defaults = deepMerge({}, options);
 
-  function icp() {}
+  function icp() { }
 
   icp.request = {};
   icp.response = {};
 
-  function icpType(prop) {
-    return function (callback, error) {
-      icp[prop].callback = callback;
-      icp[prop].error = error;
-    };
-  }
+  const icpType = (prop) => (callback, error) => {
+    icp[prop].callback = callback;
+    icp[prop].error = error;
+  };
 
   icp.request.use = icpType('request');
   icp.response.use = icpType('response');
 
-  function dataHelper(data, headers) {
+  const dataHelper = (data, headers) => {
     if (headers && headers['Content-Type']) {
       if (headers['Content-Type'] !== 'application/json') {
         return data;
@@ -58,85 +59,67 @@ export default (function create(options) {
     return data && JSON.stringify(data);
   }
 
-  function body(_opts) {
-    return {
-      method: _opts.method,
-      headers: _opts.headers,
-      body: dataHelper(_opts.data, _opts.headers),
-      signal: _opts.cancelToken,
-      credentials: _opts.withCredentials ? 'same-origin' : 'omit',
-    };
-  }
+  const body = (_opts) => ({
+    method: _opts.method,
+    headers: _opts.headers,
+    body: dataHelper(_opts.data, _opts.headers),
+    signal: _opts.cancelToken,
+    credentials: _opts.withCredentials ? 'include' : 'same-origin',
+  })
 
-  function callFetch(opts) {
-    return fetch(opts.url, body(opts)).then((res) => {
-      res.config = opts;
 
-      function reject(err) {
-        return icp.response.error
-          ? icp.response.error(err)
-          : Promise.reject(err);
-      }
+  const callFetch = (opts) => fetch(opts.url, body(opts)).then((res) => {
+    res.config = opts;
 
-      function toCompletion() {
-        // most requests respond with a body, but head requests don't, handle both cases
-        if (res.body) {
-          return opts.responseType == 'stream'
-            ? Promise.resolve(res.body)
-            : opts.responseType
+    const reject = (err) => icp.response.error
+      ? icp.response.error(err)
+      : Promise.reject(err);
+
+
+    const toCompletion = () => {
+      // most requests respond with a body, but head requests don't, handle both cases
+      if (res.body) {
+        return opts.responseType == 'stream'
+          ? Promise.resolve(res.body)
+          : opts.responseType
             ? res[opts.responseType]()
             : res.json();
-        }
-        return Promise.resolve();
       }
+      return Promise.resolve();
+    }
 
-      /* decide whether to resolve or reject based on custom validate status boolean
-       or response status codes
-      */
-      const ok = opts.validateStatus ? opts.validateStatus(res.status) : res.ok;
-      if (ok) {
-        // most http requests have to send response data of some sort, handle it
-        const hasData = toCompletion();
+    /* decide whether to resolve or reject based on custom validate status boolean
+     or response status codes
+    */
 
-        return hasData.then(
-          (data) => {
-            res.data = data;
-            return icp.response.callback
-              ? Promise.resolve(icp.response.callback(res))
-              : Promise.resolve(res);
-          },
-          (error) => reject(error)
-        );
-      }
-      return reject(res);
-    });
-  }
+    const ok = opts.validateStatus ? opts.validateStatus(res.status) : res.ok;
+    if (ok) {
+      // most http requests have to send response data of some sort, handle it
+      const hasData = toCompletion();
+      return hasData.then(
+        (data) => {
+          res.data = data;
+          if (icp.response.callback) {
+            icp.response.callback(res)
+          }
+          return Promise.resolve(res);
+        },
+        (error) => reject(error)
+      );
+    }
 
-  function request(opts) {
-    const url = defaults.baseUrl;
+    return reject(res);
+  });
+
+  const request = (opts) => {
     const config = icp.request.callback
       ? icp.request.callback(deepMerge({}, opts))
       : opts;
+
     const temp = deepMerge(defaults, config);
-    temp.url = url ? url + opts.url : opts.url;
+    // TODO : serialize url here
+    temp.url = defaults?.baseUrl ? defaults?.baseUrl + temp.url : temp.url;
     return callFetch(temp);
-  }
-
-  function lit(config) {
-    return request(config);
-  }
-
-  // Id for post, put, patch requests with data as possible payload
-  const s = 'set';
-
-  function m(method, type) {
-    return function (url, data, config) {
-      if (!config && type !== s) {
-        config = data;
-        data = null;
-      }
-      return request(deepMerge({ url, data, method }, config));
-    };
   }
 
   function cancelToken() {
@@ -153,6 +136,16 @@ export default (function create(options) {
     return is;
   };
 
+  function lit(config) {
+    return request(config);
+  }
+
+  //handle no-body verbs
+  const m = (method, payload) => payload ?
+    (url, data, config) => request(deepMerge({ url, method, data }, (config || {})))
+    :
+    (url, config) => request(deepMerge({ url, method }, (config || {})));
+
   lit.request = request;
   lit.create = create;
   lit.interceptors = icp;
@@ -162,9 +155,9 @@ export default (function create(options) {
   lit.delete = m('DELETE');
   lit.head = m('HEAD');
   lit.options = m('OPTIONS');
-  lit.post = m('POST', s);
-  lit.put = m('PUT', s);
-  lit.patch = m('PATCH', s);
+  lit.post = m('POST', payload);
+  lit.put = m('PUT', payload);
+  lit.patch = m('PATCH', payload);
 
   return lit;
 })();
